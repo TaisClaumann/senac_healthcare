f674154f9abdab9b24c04c63af014063996a2a4f
Taís Marcolino <tais.marcolino@gmail.com>
2024-06-21 21:17:45 -0300

    pasta dto

---
+++ src/main/java/com/backend_senac/healthcare/domain/dto/PacienteDto.java
@@ -0,0 +1,4 @@
+package com.backend_senac.healthcare.domain.dto;
+
+public class PacienteDto {
+}
---
+++ src/main/java/com/backend_senac/healthcare/repository/PacienteRepository.java
@@ -0,0 +1,10 @@
+package com.backend_senac.healthcare.repository;
+
+import com.backend_senac.healthcare.domain.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+}
