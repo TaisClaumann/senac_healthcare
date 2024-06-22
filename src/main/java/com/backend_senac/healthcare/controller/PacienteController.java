4de2021de3c8400383be77251b164a5d68af1a00
Taís Marcolino <tais.marcolino@gmail.com>
2024-06-21 21:38:17 -0300

    Criação de exceptions

--- src/main/java/com/backend_senac/healthcare/controller/PacienteController.java
+++ src/main/java/com/backend_senac/healthcare/controller/PacienteController.java
@@ -1,9 +1,23 @@
 package com.backend_senac.healthcare.controller;
 
+import com.backend_senac.healthcare.domain.Paciente;
+import com.backend_senac.healthcare.domain.dto.PacienteDto;
+import com.backend_senac.healthcare.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @PostMapping
+    public PacienteDto salvar(@RequestBody Paciente paciente) {
+        return pacienteService.salvar(paciente);
+    }
 }
--- src/main/java/com/backend_senac/healthcare/service/PacienteService.java
+++ src/main/java/com/backend_senac/healthcare/service/PacienteService.java
@@ -1,11 +1,10 @@
 package com.backend_senac.healthcare.service;
 
 import com.backend_senac.healthcare.domain.Paciente;
+import com.backend_senac.healthcare.domain.dto.PacienteDto;
 import com.backend_senac.healthcare.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PacienteService {
@@ -13,8 +12,7 @@ public class PacienteService {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-//    @PostMapping
-//    public Paciente salvar(@RequestBody Paciente paciente) {
-//
-//    }
+    public PacienteDto salvar(Paciente paciente) {
+        return new PacienteDto(pacienteRepository.save(paciente));
+    }
 }
